6d0ede6c44669c4e60fbc733b6919f5b5256abd1
Renan Soares <renanalencar313@gmail.com>
2025-12-17 11:38:51 -0300

    criação da entidade Partida e PartidaDao

--- app/src/main/java/br/com/fsamuel/futguess/data/AppDatabase.kt
+++ app/src/main/java/br/com/fsamuel/futguess/data/AppDatabase.kt
@@ -4,9 +4,10 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-@Database(entities = [Usuario::class], version = 1)
+@Database(entities = [Usuario::class, Partida::class], version = 2)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun usuarioDao(): UsuarioDao
+    abstract fun partidaDao(): PartidaDao
     companion object { 
         @Volatile
         private var INSTANCIA: AppDatabase? = null
@@ -16,7 +17,9 @@ abstract class AppDatabase : RoomDatabase() {
                     context.applicationContext,
                     AppDatabase::class.java,
                     "futguess_db"
-                ).build()
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
                 INSTANCIA = instance
                 instance
             }
